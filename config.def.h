01404b7a5f12ecc0983940b648bb397d8e87237d
maciekMerkury <maciekmerkury2@gmail.com>
2020-12-20 13:47:19 +0100

    Added xlock keyboard shortcut

--- config.def.h
+++ config.def.h
@@ -62,6 +62,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "rofi", "-show", "drun" };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *browsercmd[] = { "brave", NULL };
+static const char *lockcmd[] = { "xlock", NULL };
 
 #include "movestack.c"
 static Key keys[] = {
@@ -70,6 +71,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,			XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,			XK_b,      spawn,	   {.v = browsercmd } },
+	{ MODKEY|ControlMask,		XK_l,      spawn,	   {.v = lockcmd } },
 	{ MODKEY|ShiftMask,		XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
